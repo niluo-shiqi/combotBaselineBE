4200f20d4c5076fbc4b9b188aa43f59964f43f94
EC2 Default User <ec2-user@ip-172-31-2-203.us-east-2.compute.internal>
2025-06-29 08:41:20 +0000

    Fixed missing files in chatbot

---
+++ chatbot/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from .views import ChatAPIView, InitialMessageAPIView, ClosingMessageAPIView, \
+    LuluInitialMessageAPIView, LuluClosingMessageAPIView, LuluAPIView, RandomEndpointAPIView
+
+urlpatterns = [
+    path('random/', RandomEndpointAPIView.as_view(), name='random_endpoint'),
+    path('random/initial/', RandomEndpointAPIView.as_view(), name='random_initial'),
+    path('random/closing/', RandomEndpointAPIView.as_view(), name='random_closing'),
+    path('chatbot/', ChatAPIView.as_view(), name='chatbot_api'),
+    path('chatbot/initial/', InitialMessageAPIView.as_view(), name='initial_message'),
+    path('chatbot/closing/', ClosingMessageAPIView.as_view(), name='closing_message'),
+    path('lulu/initial/', LuluInitialMessageAPIView.as_view(), name='lulu_initial_message'),
+    path('lulu/closing/', LuluClosingMessageAPIView.as_view(), name='lulu_closing_message'),
+    path('lulu/', LuluAPIView.as_view(), name='lulu_chatbot_api'),
+]
